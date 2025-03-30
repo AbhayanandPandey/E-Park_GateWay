6e13728c374846f40eec5373cd427b56f88219f2
Abhay <itsabhayanand@gmail.com>
2025-03-30 02:44:24 +0000

    updated

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,12 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/CPDatabase")
-.then(()=>
-{
-    console.log("connection successful")
-})
-.catch((err)=>
-{
-    console.log(err)
-})
-
-// const sh = 
+const { DB_URL } = require("../config/env");
+mongoose.connect(DB_URL)
+.then(()=>{console.log("connection successful")})
+.catch((err)=>{console.log(err)})
--- src/models/registers.js
+++ src/models/registers.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { log } = require('handlebars');
 const UserSchema = new mongoose.Schema
 ({
     User_name:
@@ -17,8 +19,39 @@ const UserSchema = new mongoose.Schema
     {
         type:String,
         required:true
-    }
+    },
+    lastLogin: { type: Date, default: null },
+    tokens:[{
+        token:{
+            type:String,
+            required:true
+        }
+    }]
 });
 
-const Register = new mongoose.model('userdata',UserSchema);
+
+// password hashing
+
+UserSchema.pre("save", async function(next){
+    if(this.isModified("password"))
+    {
+        console.log(this.password)
+        this.password =await bcrypt.hash(this.password,10)
+        console.log(this.password)
+    }
+    next()
+})
+// tokens 
+UserSchema.methods.generateAuthToken = async function () {
+    try {
+        const token = jwt.sign({_id:this._id.toString()},"token")
+        this.tokens = this.tokens.concat({token:token});
+        await this.save();
+        return token;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const Register = new mongoose.model('userdata1',UserSchema);
 module.exports = Register;
