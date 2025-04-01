69c50abca9df7a2dcb564db89dc8a0b374bdaa13
Abhay <itsabhayanand@gmail.com>
2025-04-01 06:02:31 +0530

    Updated

--- src/middleware/redirect.js
+++ src/middleware/redirect.js
@@ -1,7 +1,7 @@
 function authMiddleware(req, res, next) {
     if (!G_A.ga) { 
-      req.session.redirectTo = req.originalUrl; // Store attempted route
-      return res.redirect("/loginsignup"); // Redirect to login page
+      req.session.redirectTo = req.originalUrl; 
+      return res.redirect("/loginsignup");
     }
-    next(); // If logged in, continue to requested route
+    next();
   }
