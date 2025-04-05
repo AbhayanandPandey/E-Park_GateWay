35635388f6c471dc0436dbcfc3aca05fc952f7b5
Abhay <itsabhayanand@gmail.com>
2025-04-05 12:21:30 +0530

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
-module.exports = {
+module.exports = {  
   PORT: process.env.PORT || 3000,
   DB_URL: process.env.DB_URL || "mongodb://pathofyourDB",
 };
