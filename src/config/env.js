c97793b0aabcc059e7775d64915ab8a4149cef37
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-04-25 20:55:30 +0530

    updated

--- src/config/env.js
+++ src/config/env.js
@@ -4,4 +4,4 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   DB_URL: process.env.DB_URL || "mongodb+srv://eparkgateway:qsb3O2PbQzMUysvX@epark.vrqna2i.mongodb.net/NEWDATA?retryWrites=true&w=majority&appName=Epark", 
 
-};  
+};   
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const { DB_URL } = require("../config/env");
-mongoose.connect(DB_URL)
-.then(()=>{console.log("connection successful")})
-.catch((err)=>{console.log(err)})
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => {
+  console.log('Failed to connect to MongoDB, retrying in 5 seconds...', err);
+  setTimeout(connectWithRetry, 5000);
+}); 
